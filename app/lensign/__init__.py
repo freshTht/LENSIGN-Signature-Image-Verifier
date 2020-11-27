8dbb42892ca3bdedad23d986f792310ccc553670
fresh.thit@me.com <fresh@SALLY-Matebook.localdomain>
2020-11-27 14:10:57 +0700

    remove tensorflow & lstm models

--- app/lensign/__init__.py
+++ app/lensign/__init__.py
@@ -1,5 +1,5 @@
 import json
-from .controllers import ls_models, ls_firebase
+from .controllers import ls_firebase
 from . import ls_image_classifier
 
 import flask
@@ -24,63 +24,6 @@ def kill_server():
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
-
-@app.route('/model/classify', methods=['GET', 'POST'])
-def test():
-  json = request.get_json()
-  x = json['x']
-  n = len(x)
-
-  # equal-width sampling
-  REQUIRED_N = ls_models.get_input_shape()[0]
-  step = (n-1) / (REQUIRED_N-1)
-  decimal_i = 0
-
-  indexes = []
-  sampled_x = []
-  while len(indexes) < REQUIRED_N:
-    i = int(decimal_i)
-    indexes.append(i)
-    sampled_x.append(x[i])
-    decimal_i += step
-
-  # y = ls_models.predict(x)
-  sampled_y = ls_models.predict(sampled_x)
-  return flask.jsonify({
-    'success': True,
-    'data': {
-      # 'raw': {
-      #   'input-size': n,
-      #   'result': y,
-      # },
-      'sampled': {
-        'input-size': len(indexes),
-        'result': sampled_y,
-      }
-    },
-  })
-
-@app.route('/model/input-shape', methods=['GET'])
-def input_shape():
-  input_shape = ls_models.get_input_shape()
-  return flask.jsonify({
-    'success': True,
-    'data': input_shape,
-  })
-
-@app.route('/test-params', methods=['GET', 'POST'])
-def test_params():
-  json = request.get_json()
-  form_data = request.form    # body (form data)
-  params = request.args       # args (in url)
-  return flask.jsonify({
-    'success': True,
-    'data': {
-      'json': json,
-      'form_data': form_data,
-      'params': params,
-    }
-  })
 
 #
 # SigVer (Image)
@@ -126,60 +69,6 @@ def sigver_classify(user_id):
       'success': True,
       'data': res,
     })
-
-#
-# Firebase
-#
-@app.route('/training/datasets', methods=['GET'])
-def datasets_list():
-  result = ls_firebase.datasets.list_all()
-
-  return flask.jsonify({
-    'success': True,
-    'data': result
-  })
-
-@app.route('/training/datasets', methods=['POST'])
-def datasets_new():
-  json = request.get_json()
-  form_data = request.form    # body (form data)
-
-  if json != None:
-    result = ls_firebase.datasets.add(json['name'])
-  else:
-    result = ls_firebase.datasets.add(form_data['name'])
-
-  return flask.jsonify({
-    'success': True,
-    'data': result
-  })
-
-@app.route('/training/datasets/<id>/signatures', methods=['GET'])
-def datasets_list_signatures(id):
-  result = ls_firebase.datasets.list_signatures(id)
-  return flask.jsonify({
-    'success': True,
-    'data': result
-  })
-
-@app.route('/training/datasets/<id>/signatures', methods=['POST'])
-def datasets_new_signature(id):
-  json = request.get_json()
-  result = ls_firebase.datasets.add_signature(id, json['signature'])
-  return flask.jsonify({
-    'success': True,
-    'data': result
-  })
-
-# WIP
-# @app.route('/training/datasets/<id>/signatures/execute', methods=['POST'])
-# def datasets_new_signature(id):
-#   json = request.get_json()
-#   result = ls_firebase.datasets.add_signature(id, json['signature'])
-#   return flask.jsonify({
-#     'success': True,
-#     'data': result
-#   })
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
